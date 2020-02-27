ac775f92973b6d619cf022c970f0f2a40ba9b1dd
ehdgus8077 <drea123@naver.com>
2020-02-28 01:21:03 +0900

    feat: 기본적인 로그인 기능 추가

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
-import { Logger } from "./common/logger"
 import * as constants from "./common/constants";
 import * as program from "commander";
+import { Server } from "./server/server";
+import { getDatabase } from "./database/database";
 
-const log = Logger.createLogger("index");
 
 program.version(constants.VERSION);
 
 program.command("serve").action(async () => {
+    getDatabase().sync();
+    const server = new Server();
+    server.start();
 });
 
 program.parse(process.argv);
