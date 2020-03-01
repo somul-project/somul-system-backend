82ce9b2e78fd6335e73ede7f594c628e839ff9df
ehdgus8077 <drea123@naver.com>
2020-03-01 14:10:22 +0900

    feat: type-graphql를 이용하여 타이핑 추가

---
+++ src/api/graphql/objectTypes/MutationRes.ts
@@ -0,0 +1,11 @@
+import { ObjectType, Field } from 'type-graphql';
+
+@ObjectType()
+export class MutationRes {
+  
+  @Field({ nullable: true })
+  result?: boolean;
+
+  @Field({ nullable: true })
+  error?: string;
+}
--- src/server/server.ts
+++ src/server/server.ts
@@ -3,14 +3,10 @@ import * as constants from "../common/constants";
 import { Logger } from "../common/logger";
 import * as session from "express-session";
 import * as  bodyParser from 'body-parser';
-import { User } from "../database/user.model";
-// import * as sendgrid from "../common/sendgrid";
-
-const loginRouter = require('../api/login');
-const emailRouter = require('../api/email');
-const graphqlRouter = require('../api/graphql');
+import getGraphQlserver from "../api/graphql";
 
 const log = Logger.createLogger("server.server");
+const loginRouter = require('../api/login');
 
 export class Server {
   private app: express.Application;
@@ -18,17 +14,16 @@ export class Server {
   constructor() {
     this.app = express();
   }
-  public start() {
-    this.addEvent();
+  public async start() {
+    await this.addEvent();
     this.app.listen(constants.SERVER_PORT, 
       () => log.info(`Example app listening on port ${constants.SERVER_PORT}!`))
   }
   public authenticateUser = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    console.log(req.session)
     if (req.session && req.session.passport && req.session.passport.user !== undefined) {
       if (req.session.passport.user.admin === undefined) {
-        res.render("infomation.html");
-
-        // res.send({code: -2});
+        res.send({code: -2});
       } else {
         next();
       }
@@ -37,7 +32,7 @@ export class Server {
     }
   };
 
-  private addEvent() {
+  private async addEvent() {
     this.app.use(bodyParser.json()); 
     this.app.use(bodyParser.urlencoded({ extended: true })); 
     this.app.use(session({
@@ -46,54 +41,13 @@ export class Server {
       resave: true,
       saveUninitialized: false
     }));
-    this.app.use('/login', loginRouter);
-    this.app.use('/graphql', this.authenticateUser, graphqlRouter);
-    this.app.use('/email', this.authenticateUser, emailRouter);
-    this.app.set('view engine', 'ejs');
-    this.app.engine('html', require('ejs').renderFile);
-    
 
     this.app.get('/', this.authenticateUser , async (req, res, next) => {
       res.send({result: 0});
     });
-    this.app.get('/info', async (req, res, next) => {
-      res.render("infomation.html");
-    });
-    this.app.get('/local_login', async (req, res, next) => {
-      res.render("local_login.html");
-    });
-    this.app.get('/reg', async (req, res, next) => {
-      res.render("register.html");
-    });
-    
-    this.app.post('/register', async (req, res, next) => {
-      const info = req.body;
-      let user;
-      if (req.session && req.session.passport &&req.session.passport.user !== undefined) {
-      // OAuth
-        user = User.build({
-          email: req.session.passport.user!["email"],
-          name: info.name,
-          phone_number: info.phone_number,
-          admin: false,
-        });
-      } else {
-        // local
-        // @TODO verify email.
-        user = User.build({
-          email: info.email,
-          password: info.password,
-          name: info.name,
-          phone_number: info.phone_number,
-          admin: false,
-        });
-      }
-      try {
-        await user.save();
-        res.send({result: 0});
-      } catch (e) {
-        res.send({result: -1, error: e});
-      }
-    });
+
+    this.app.use('/login', loginRouter);
+    const graphQlserver = await getGraphQlserver();
+    this.app.use("/graphql", graphQlserver);
   }
 }
